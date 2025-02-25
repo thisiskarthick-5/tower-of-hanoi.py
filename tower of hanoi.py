91fa7a71e2c6478d7d0b30a3c779c99c248c379b
karthick <karthickfaf19.7@gmail.com>
2025-02-25 21:18:06 +0530

    Update tower of hanoi.py

--- tower of hanoi.py
+++ tower of hanoi.py
@@ -1,1 +1,16 @@
+disc = int(input("ENTER THE NUMBER OF DISC  :"))
+source = 'A'
+aux = 'B'
+dest = 'C'
+
+def towerofhanoi(disc,source,dest,aux):
+    if disc == 1:
+        print(f"MOVE DISC {source} from {dest}")
+    else:
+        towerofhanoi(disc-1,source,aux,dest)
+        print(f"MOVE DISC {source} from {dest}")
+        towerofhanoi(disc-1,aux,dest,source)
+
+
+towerofhanoi(disc,source,dest,aux)
 
